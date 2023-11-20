c8e03e5089997c9473dd715cf580cc36a2cdb676
pzj <ahopeff@gmail.com>
2023-11-20 16:55:22 +0800

    Remove files from repository but keep them locally

--- bonus/bonus10_function.py
+++
@@ -1,8 +0,0 @@
-def parse(user_input):
-
-    user_input = user_input.split(",")
-    lower_bound = int(user_input[0])
-    upper_bound = int(user_input[1])
-
-    return {"lower_bound": lower_bound, "upper_bound": upper_bound}
-
